b1cc2da8080e355fce4321b3330de2b1a39ad3be
olshanskiyoleksii <60468619+medvedaleksey@users.noreply.github.com>
2021-08-08 11:59:18 +0300

    Add files via upload

---
+++ task-2.js
@@ -0,0 +1,34 @@
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+console.log('======');
+console.log('Task 2');
+console.log('======');
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action.call(this, itemName);
+};
+
+invokeInventoryAction.call(inventory, 'Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction.call(inventory, 'Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
